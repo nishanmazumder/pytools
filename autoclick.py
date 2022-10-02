b68f9f1620fd3d8e53e099a8a3b6350ff82631a3
nagnagini <nagnagini@nagini.nag>
2022-10-02 11:57:47 -0400

    pytools for click

---
+++ autoclick.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://mysite.com")
+links = driver.find_elements(By.TAG_NAME, "link")
+scripts = driver.find_elements(By.TAG_NAME, "script")
+
+for link in links:
+    href = link.get_attribute("href")
+    driver.execute_script("window.open('"+href+"')")
+
+    print(link.get_attribute("href"))
+    # break
+
+for script in scripts:
+    src = script.get_attribute("src")
+    driver.execute_script("window.open('"+src+"')")
+
+    print(script.get_attribute("src"))
+    # break
+
+driver.quit()
+
+print('done!')
